a749bcd50fd559654eac11cc060be1c8ee6a7409
Jose Luis <admin@huehue.mx>
2022-04-23 00:22:47 -0500

    Added a basic configuration to add the auth service

--- ApiGateway/src/Program.cs
+++ ApiGateway/src/Program.cs
@@ -1,6 +1,28 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Adding Ocelot service
+builder.Services.AddOcelot();
+
+
+//Adding the Logging
+builder.Host.ConfigureLogging((hostingContext, logginBuilder) =>
+{
+    logginBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    logginBuilder.AddConsole();
+    logginBuilder.AddDebug();
+});
+
+builder.Host.ConfigureAppConfiguration((hosting, config) =>
+{
+    config.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+});
+
 var app = builder.Build();
 
+app.UseOcelot().Wait();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
