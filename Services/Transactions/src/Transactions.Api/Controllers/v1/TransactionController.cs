c40db22e7636498893693b1a6d2913d632442439
Jose Luis <admin@huehue.mx>
2022-05-01 11:46:39 -0500

    Added baseapi and transaction controller

---
+++ Services/Transactions/src/Transactions.Api/Controllers/v1/TransactionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Transactions.Application.Features.Transactions.Commands.CreateTransaction;
+
+namespace Transactions.Api.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class TransactionController : BaseApiController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateTransactionCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
